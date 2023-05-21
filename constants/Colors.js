9745dbf9bf822c1728b7abdf87ef1d10c10a24ad
Shubh Shah <shahshubh1010@gmail.com>
2023-05-21 07:33:40 +0000

    cleanup

--- constants/Colors.js
+++
@@ -1,7 +0,0 @@
-export default {
-    primary: '#d268ee',
-    lightPrimary: '#f26df1',
-    accent: '#4442b7',
-    lightAccent: '#7b61f3',
-    brightBlue: '#5260f2'
-}
--- constants/VerifiedUser.js
+++
@@ -1,5 +0,0 @@
-export default {
-    // verifiedUsersEmail: ["shubh.as@somaiya.edu", "test@abc.com"],
-    verifiedUsersId: ["5ff208bebdc8a700041dd9c5", "5eb91f85a8003946701a2ebd"],
-    
-}
